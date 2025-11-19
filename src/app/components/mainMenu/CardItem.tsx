235740d858d46b31777f9e59df5b61b6aba4bea9
Vsevolod Berdutin <vsevolodberdutin@gmail.com>
2025-11-19 17:21:21 +0300

    relokate center

--- src/app/components/mainMenu/CardItem.tsx
+++ src/app/components/mainMenu/CardItem.tsx
@@ -14,7 +14,7 @@ export const CardItem: React.FC<MenuItemProps> = ({ id, title, subtitle }) => {
     e.preventDefault()
     const element = document.getElementById(id)
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      element.scrollIntoView({ behavior: 'smooth', block: 'center' })
     }
   }
 
