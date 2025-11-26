30b4427df8434105f6932c606cb82454358c670a
Vsevolod Berdutin <vsevolodberdutin@gmail.com>
2025-11-27 02:02:58 +0300

    fix: wrapper py

--- src/app/uiElements/wrappers/GridWrapper.tsx
+++ src/app/uiElements/wrappers/GridWrapper.tsx
@@ -9,5 +9,5 @@ export const GridWrapper: React.FC<GridWrapperProps> = ({ children }) => (
   <div className="h-fit w-full lg:px-10 px-5 py-10 sp-10 grid grid-cols-1 lg:grid-cols-[450px_850px] justify-center items-start gap-5">{children}</div>
 )
 export const GridProductWrapper: React.FC<GridWrapperProps> = ({ children }) => (
-  <div className="h-fit w-full lg:px-10 px-5 py-10 grid grid-cols-1 lg:grid-cols-[450px_850px] justify-center items-start gap-5">{children}</div>
+  <div className="h-fit w-full lg:px-10 px-5 py-1 grid grid-cols-1 lg:grid-cols-[450px_850px] justify-center items-start gap-5">{children}</div>
 )
